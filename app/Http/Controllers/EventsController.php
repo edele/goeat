4e80cfcac05c3840d4514ad7502adefc474c107f
Edele Gizatullin <me@edele.ru>
2016-03-06 01:01:51 +0500

    create, read and update events

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Event;
+
+class EventsController extends Controller
+{
+    public function index()
+    {
+        $events = Event::all();
+        return $events;
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+
+        return $event;
+    }
+
+    public function store(Request $request)
+    {
+    	$fields = $request->all();
+        $event = new Event($fields);
+
+        $event->save();
+
+        return ['id' => $event->id];
+    }
+
+    public function update(Request $request, $id)
+    {
+        $fields = $request->all();
+        $event = Event::findOrFail($id);
+
+        $event->update($fields);
+
+        return [];
+    }
+}
