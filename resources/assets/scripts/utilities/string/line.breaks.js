84ebdb4eff8ba80780d92d6c43475cc2474af5e1
Edele Gizatullin <me@edele.ru>
2016-03-17 22:20:03 +0500

    strip tags before inserting <br> tags

--- resources/assets/scripts/utilities/string/line.breaks.js
+++ resources/assets/scripts/utilities/string/line.breaks.js
@@ -1,5 +1,8 @@
 'use strict'
 
+import strip from './strip.tags'
+
 export default function (text) {
+    text = strip(text)
     return text.replace(/\n{2,}/g, '<br><br>').replace(/\n/g, '<br>')
 }
